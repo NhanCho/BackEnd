099a25abdcef62c45b261091a57ae49ae927ed54
Nhan1311 <nhan.pt15049@sinhvien.hoasen.edu.vn>
2023-02-25 02:02:59 +0700

    push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
 
+import{ UserService } from './service/service.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AngularForJs';
+  constructor(private userService: UserService) { }
+
+  item$: any = new Observable<Object>
+  id: string = '';
+  name: string = '';
+  age: string = '';
+
+
+  getAll()
+  {
+    this.item$ = this.userService.getAll();
+  }
+
+  getCreate()
+  {
+    this.userService.getCreateItem({
+      id: this.id,
+      name: this.name,
+      age: this.age
+    })
+    this.id = '';
+    this.name = '';
+    this.age = '';
+  }
 }
+
---
+++ src/app/service/service.service.ts
@@ -0,0 +1,18 @@
+import { environment } from './../../environments/environment';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor(private http: HttpClient) { }
+
+  getAll(){
+    return this.http.get(`${environment.api_url}all`);
+  }
+
+  getCreateItem(body: any){
+    return this.http.post(`${environment.api_url}create`, body).subscribe();
+  }
+}
